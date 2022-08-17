1202422f10680eabf0613c26a4db722b69d3b200
David Duncan <devduncan89@gmail.com>
2022-08-17 16:44:17 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+	import Nav from '../lib/nav/Nav.svelte';
+</script>
+
+<Nav />
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		@apply max-w-7xl mx-auto bg-neutral-800;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-3xl font-bold text-center">About</h1>
+
+<p>Hello, about this page. its not done, thats all there is to know about</p>
